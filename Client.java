0522df767837f185cbc7ac9b62eaf6151e3bc7a8
Amrutha <96amruthagopinath@gmail.com>
2018-03-27 14:52:20 +0530

    done

--- Client.java
+++ Client.java
@@ -7,3 +7,7 @@ public static void main(String args[]) throws Exception
       BufferedReader inFromUser =
       new BufferedReader(new InputStreamReader(System.in));
       DatagramSocket clientSocket = new DatagramSocket();
+InetAddress IPAddress = InetAddress.getByName("localhost");
+      byte[] sendData = new byte[1024];
+      byte[] receiveData = new byte[1024];
+      boolean flag=true;
