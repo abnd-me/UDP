654ba64596c8883115ecc89e34b9191f9e661c1b
Amrutha <96amruthagopinath@gmail.com>
2018-03-27 14:43:17 +0530

    committed

--- Client.java
+++ Client.java
@@ -2,3 +2,8 @@ import java.io.*;
 import java.net.*;
 class Client
 {
+public static void main(String args[]) throws Exception
+   {
+      BufferedReader inFromUser =
+      new BufferedReader(new InputStreamReader(System.in));
+      DatagramSocket clientSocket = new DatagramSocket();
