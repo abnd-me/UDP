13477685427f75a6cd37ea8db57e9c2d1ede0c62
Abhinand <abhinandpp98@gmail.com>
2018-03-27 14:42:57 +0530

    new line added

--- Server.java
+++ Server.java
@@ -4,3 +4,10 @@ import java.net.*;
 class UDPServer
 {
    public static void main(String args[]) throws Exception
+{
+         DatagramSocket serverSocket = new DatagramSocket(9876);
+            byte[] receiveData = new byte[1024];
+            byte[] sendData = new byte[1024];
+            while(true)
+               {
+                  DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
